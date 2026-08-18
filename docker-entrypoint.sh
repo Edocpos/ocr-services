8b30f1ce1a552b93d2f30f35bebb2bd3a438b3ba
DyDxdYdX <dexterdykestimothy@gmail.com>
2026-08-18 11:18:36 +0800

    feat: add artisan configuration, route, and view caching to docker entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -11,5 +11,8 @@ mkdir -p storage/framework/views \
 
 php artisan optimize:clear || true
 chmod -R 775 storage bootstrap/cache || true
+php artisan config:cache
+php artisan route:cache
+php artisan view:cache
 
 exec php artisan serve --host=0.0.0.0 --port=8000
